b51ef39c04091009d01d6c0b1521ae5527659e2f
Dulrix <dulrixhq@gmail.com>
2026-01-30 17:15:15 +0400

    Add Band Name Generator script

---
+++ Day02_BandNameGenerator/Band Name Generator.py
@@ -0,0 +1,13 @@
+
+
+# Print welcome message
+print("Welcome to the Band Name Generator!")
+# Get user's hometown
+city = input("What's the name of the city you grew up in?\n")
+# Get user's pet name
+pet = input("What's your pet's name?\n")
+# Combine city and pet to create band name
+band_name = city + " " + pet
+# Print potential band name
+print("Your band name could be: " + band_name)
+
